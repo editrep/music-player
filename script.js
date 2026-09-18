c72481d818eaa8c5fddfffe2414aad95be65fcfc
editrep <edithedowney@icloud.com>
2026-09-17 20:46:15 -0400

    add starter code from class notes to js

---
+++ script.js
@@ -0,0 +1,77 @@
+//load a song dynamically
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+const songs = ['hey', 'summer', 'ukulele'];
+let songIndex = 2;
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+loadSong(songs[songIndex]);
+
+//add play or pause controls
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.replace('fa-play', 'fa-pause');
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.replace('fa-pause', 'fa-play');
+  audio.pause();
+}
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+  isPlaying ? pauseSong() : playSong();
+});
+
+//add next and previous logic
+function prevSong() {
+  songIndex = (songIndex - 1 + songs.length) % songs.length;
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex = (songIndex + 1) % songs.length;
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+//update the progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const percent = (currentTime / duration) * 100;
+  progress.style.width = `${percent}%`;
+}
+
+audio.addEventListener('timeupdate', updateProgress);
+
+//click to seek/scrub in track
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  audio.currentTime = (clickX / width) * audio.duration;
+}
+
+progressContainer.addEventListener('click', setProgress);
+
+//auto play next song when current song ends
+audio.addEventListener('ended', nextSong);
